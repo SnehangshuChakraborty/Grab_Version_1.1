fe178573110dc8b562d0655762ac0cfce0f8aa25
Snehangshu Chakraborty <chakraborty.snehangshu@gmail.com>
2020-05-15 06:59:32 +0530

    Added Sliding Banners on headers

---
+++ app/src/main/java/com/example/shoppers_beta/ViewHolder/ViewPagerHolder.java
@@ -0,0 +1,54 @@
+package com.example.shoppers_beta.ViewHolder;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.shoppers_beta.R;
+
+
+public class ViewPagerHolder extends PagerAdapter {
+
+    private Context context;
+    private LayoutInflater layoutInflater;
+    private Integer [] images = {R.drawable.banner_a,R.drawable.banner_b};
+
+    public ViewPagerHolder(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view==object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.context_layout,null);
+        ImageView imageView = (ImageView) view.findViewById(R.id.banner_imageview);
+        imageView.setImageResource(images[position]);
+        ViewPager viewPager = (ViewPager) container;
+        viewPager.addView(view,0);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        ViewPager viewPager = (ViewPager) container;
+        View view = (View) object;
+        viewPager.removeView(view);
+    }
+}
